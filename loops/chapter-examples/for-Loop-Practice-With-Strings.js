0093f89ed6afb500bbc81cb5091803f60c28d897
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2024-06-24 16:14:00 -0400

    cheapter examples for loop practice

--- loops/chapter-examples/for-Loop-Practice-With-Strings.js
+++ loops/chapter-examples/for-Loop-Practice-With-Strings.js
@@ -1,4 +1,8 @@
 // Create a string variable containing your name.
-
+let name = "Zeb";
 
 // Write a for loop that prints each character in your name on a different line.
+for (let i = 0; i < name.length; i++)
+    {
+        console.log(name[i]);
+    }
