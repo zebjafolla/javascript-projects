558acad12ce91daaa66fd1f49078842e8d780820
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2024-06-24 16:16:19 -0400

    family names arrays loop

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,7 @@
 // create an array variable containing the names
-
+let names = ["Amy", "Leslie", "Chris", "Andy", "Jen", "Collin", "Jan", "Adam", "Wyatt", "Margo"];
 // write a for loop that prints each name on a different line
+for (let i = 0; i < names.length; i++)
+    {
+        console.log(names[i]);
+    }
