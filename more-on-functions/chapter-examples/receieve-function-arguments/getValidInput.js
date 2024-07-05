5e0334f1d494232fc87fe44ba8425a6753000cc4
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2024-07-05 05:06:09 -0400

    trying examples from reading

--- more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
+++ more-on-functions/chapter-examples/receieve-function-arguments/getValidInput.js
@@ -12,6 +12,16 @@ function getValidInput(prompt, isValid) {
     return userInput;
 }
 
+let isCharA = function(n) {
+  if (n.slice(0, 1) == "a")
+    {
+      return true;
+    }
+    else{
+      return false
+    }
+};
+getValidInput('Please insert input that starts with an "a"', isCharA)
 // TODO 1: write a validator 
 // that ensures input starts with "a"
 
@@ -19,3 +29,25 @@ function getValidInput(prompt, isValid) {
 // that ensures input is a vowel
 
 // Be sure to test your code!
+// const input = require('readline-sync');
+
+// function getValidInput(prompt, isValid) {
+
+//     // Prompt the user, using the prompt string that was passed
+//     let userInput = input.question(prompt);
+
+//     // Call the boolean function isValid to check the input
+//     while (!isValid(userInput)) {
+//     console.log("Invalid input. Try again.");
+//     userInput = input.question(prompt);
+//     }
+
+//     return userInput;
+// }
+
+// // A boolean function for validating input
+// let isEven = function(n) {
+//     return Number(n) % 2 === 0;
+// };
+
+// console.log(getValidInput('Enter an even number:', isEven));
