6afe97550aa802823c9570dee7ceb1ebebadc3ff
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2024-07-05 06:31:56 -0400

    raid a shuttle exercise

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -5,6 +5,22 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
+let calcPirate = function (input){
+    if (typeof input == "number")
+    {
+        return input * 3;
+    }
+    else if (typeof input == "string")
+    {
+        return "ARRR!";
+    }
+    else
+    {
+        return input;
+    }
+    
+};
+console.log(calcPirate(NaN));
 
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
@@ -13,3 +29,4 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+console.log(arr.map(calcPirate));
--- more-on-functions/exercises/raid-a-shuttle.js
+++ more-on-functions/exercises/raid-a-shuttle.js
@@ -8,6 +8,7 @@ function checkFuel(level) {
   }
 }
 
+
 function holdStatus(arr){
   if (arr.length < 7) {
     return `Spaces available: ${7-arr.length}.`;
@@ -28,6 +29,26 @@ console.log("Hold status: " + holdStatus(cargoHold));
  */
  
 //a). Define an anonymous function and set it equal to a variable with a normal, non-suspicious name. The function takes one parameter. This will be the fuel level on the shuttle.
+let stolenFuel = 0;
+let steal = function (n) {
+  while (checkFuel(n) === "green")
+    {
+      
+      stolenFuel += 1;
+      n -= 1;
+      fuelLevel -= 1;
+    }
+    if(checkFuel(n) === "yellow")
+      {
+        n += 1;
+        fuelLevel += 1;
+        stolenFuel -= 1;
+      }
+    return stolenFuel;
+}
+// console.log(`Stolen fuel level is ${steal(fuelLevel)}`);
+// console.log("Fuel level: " + checkFuel(fuelLevel));
+// console.log(fuelLevel)
 
 //b). You must siphon off fuel without alerting the TAs. Inside your function, you want to reduce the fuel level as much as possible WITHOUT changing the color returned by the checkFuel function.
 
@@ -37,7 +58,20 @@ console.log("Hold status: " + holdStatus(cargoHold));
 
 /* Next, liberate some of that glorious cargo.
  */
-
+let stolenCargo = [];
+let stealCargo = function(n){
+  console.log(`There is ${n} inside of the cargo hold`);
+  
+  let targets = [n.indexOf("gold"), n.indexOf("AE-35 unit")];
+  console.log(targets);
+  stolenCargo.push(n.slice(n.indexOf("gold"), n.indexOf("gold") + 1));
+  stolenCargo.push(n.slice(n.indexOf("AE-35 unit"), n.indexOf("AE-35 unit") + 1));
+  n.splice(targets[0], 1, "teddy bear");
+  n.splice(targets[1], 1, "bouncy Ball");
+  console.log(`There is ${n} inside of the cargo hold`);
+  return stolenCargo;
+}
+// console.log(`Youve stolen ${stealCargo(cargoHold)}`);
 //a). Define another anonymous function with an array as a parameter, and set it equal to another innocent variable.
 
 //b). You need to swipe two items from the cargo hold. Choose well. Stealing water ain’t gonna get us rich. Put the swag into a new array and return it from the function.
@@ -50,7 +84,13 @@ console.log("Hold status: " + holdStatus(cargoHold));
  */
  
 //a). Define a function called irs that can take fuelLevel and cargoHold as arguments.
-	
+	function irs(fuelLevel, cargoHold)
+  {
+    stealCargo(cargoHold);
+    steal(fuelLevel);
+    console.log(`Raided ${stolenFuel} kg of fuel from the tanks, and stole ${stolenCargo[0]} and ${stolenCargo[1]} from the cargo hold.`);
+  }
+  irs(fuelLevel, cargoHold);
 //b). Call your anonymous fuel and cargo functions from within irs.
 
 //c). Use a template literal to return, "Raided _____ kg of fuel from the tanks, and stole ____ and ____ from the cargo hold."
