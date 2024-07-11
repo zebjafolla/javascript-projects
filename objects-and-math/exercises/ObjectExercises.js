b90a32478e3608728d34f59244983b90255fe64d
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2024-07-10 22:17:07 -0230

    workin on it

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,14 +2,56 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronauhtId: 3,
+   move: function(){
+      return Math.round((Math.random() * 10));
+   }
 };
+console.log(superChimpOne.move());
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronauhtId: 8,
+   move: function(){
+      return Math.round((Math.random() * 10));
+   }
+};
+
+let brad = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 5,
+   astronauhtId: 10,
+   move: function(){
+      return Math.round((Math.random() * 10));
+   }
+};
+
+let leroy = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronauhtId: 2,
+   move: function(){
+      return Math.round((Math.random() * 10));
+   }
+};
+
+let almina = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronauhtId: 1,
+   move: function(){
+      return Math.round((Math.random() * 10));
+   }
 };
 
 
@@ -18,7 +60,46 @@ let salamander = {
 // Add a move method to each animal object
 
 // Create an array to hold the animal objects.
+let crew = [];
+crew.push(superChimpOne, salamander, brad, leroy, almina);
+console.log(crew);
 
 // Print out the relevant information about each animal.
+function crewReports(animal){
+   return `${animal.name} is a ${animal.species}. They are ${String(animal.age)} years old and ${animal.mass} kilograms. Their ID is ${animal.astronauhtId}.'`
+
+}
+console.log(crewReports(almina));
+
+
+function fitnessTest(testGroup)
+{
+   let steps = [0, 0, 0, 0, 0];
+   let ctr = 0;
+   let winner = false;
+   let winnerInfo;
+
+   for (let i = 0; i < testGroup.length; i++)
+      {
+         if (winner == false)
+            {
+               if(steps[i] > 20)
+                  {
+                     winner = true;
+                     winnerInfo = testGroup[i];
+                     console.log(winnerInfo)
+                     
+                  }
+               steps[i] += (testGroup[i].move());
+               ctr += 1;
+            }
+              
+      }
+
+      // return `${winnerInfo.name} took ${steps[i]} steps in ${ctr} turns to win this race.`;
+} 
+         
+
 
 // Start an animal race!
+console.log(fitnessTest(crew));
