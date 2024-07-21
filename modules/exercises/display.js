2e2ef4bdc0f41496b074f1b9573874e3e4f4121e
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2024-07-21 11:46:46 -0230

    turtle is the next spacewalker

--- modules/exercises/display.js
+++ modules/exercises/display.js
@@ -34,3 +34,5 @@ function printTestScores(index,test,students,scores){
   }
   return;
 }
+
+module.exports = printAll;
--- modules/exercises/randomSelect.js
+++ modules/exercises/randomSelect.js
@@ -1,5 +1,26 @@
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8];
+
 function randomFromArray(arr){
   //Your code here to select a random element from the array passed to the function.
+  let max = arr.length;
+  // console.log(`array.length is equal to ${arr.length} max is equal to ${max}`);
+  let min = 0;
+  let randomNumber = Math.floor(Math.random() * 10);
+  // console.log(`first random number: ${randomNumber}`);
+
+  while (randomNumber > (max - 1))
+  {
+    randomNumber = Math.floor(Math.random() * 10);
+    // console.log(`second random number: ${randomNumber}`);
+  }
+  
+  return arr[randomNumber];
 }
 
+// console.log(randomFromArray(array))
 //TODO: Export the randomFromArray function.
+
+module.exports = randomFromArray;
+
+
