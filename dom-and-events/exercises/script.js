f7749cb5ce73dce0d73e7905b8666b8f04855ccc
zebjafolla <113193465+zebjafolla@users.noreply.github.com>
2024-08-05 17:46:26 -0400

    trying again

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,6 +4,22 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
+    button.addEventListener("click", function(){
+        paragraph.innerHTML = "Houston, we have liftoff!"
+    });
+    missionAbort.addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "red";
+    });
+    missionAbort.addEventListener("mouseout", () => {
+        missionAbort.style.removeProperty('background-color');
+    })
+    missionAbort.addEventListener("click", function(){
+        let answer = window.confirm("Are you sure you want to abort the mission?");
+        if(answer === true)
+        {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+    })
     
 }
 
